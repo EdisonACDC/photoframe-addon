93ba30ebdb68e2173fd9fc8314544c9afd816164
EdisonACDC <77065985+EdisonACDC@users.noreply.github.com>
2025-10-24 23:31:06 +0200

    Add photoframe/run.sh

---
+++ photoframe/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "Avvio PhotoFrame Add-on..."
+
+# Leggi configurazione
+UPLOAD_LIMIT=$(bashio::config 'upload_limit')
+MAX_FILE_SIZE=$(bashio::config 'max_file_size')
+
+bashio::log.info "Limite upload: ${UPLOAD_LIMIT} file"
+bashio::log.info "Dimensione massima file: ${MAX_FILE_SIZE}MB"
+
+# Crea directory uploads se non esiste
+mkdir -p /data/uploads
+
+# Export variabili ambiente
+export UPLOAD_DIR=/data/uploads
+export PORT=5000
+
+cd /app
+
+# Avvia l'applicazione
+bashio::log.info "PhotoFrame avviato su porta 5000"
+exec npm start
